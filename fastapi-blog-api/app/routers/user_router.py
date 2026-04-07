7e98be1a3baf69bbcd83ac3ac01dfc2b5fad5c84
devAO-bit <abhitech.work@gmail.com>
2026-04-07 13:24:40 +0530

    Implement user authentication and password hashing; add login endpoint and security utilities

--- fastapi-blog-api/app/routers/user_router.py
+++ fastapi-blog-api/app/routers/user_router.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
 
 from app.database import get_db
 from app.services import user_service
 from app.schemas.user_schema import UserCreate, UserResponse
+
+from app.schemas.user_schema import UserLogin
+
 
 router = APIRouter()
 
@@ -18,3 +22,18 @@ def get_users(db: Session = Depends(get_db)):
 
     return user_service.get_all_users(db)
     
+
+
+@router.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+
+    authenticated_user = user_service.authenticate_user(
+        db, 
+        user.email,
+        user.password
+    )
+
+    if not authenticated_user:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    return {"message": "Login successful"}
--- fastapi-blog-api/app/services/user_service.py
+++ fastapi-blog-api/app/services/user_service.py
@@ -2,11 +2,19 @@ from sqlalchemy.orm import Session
 from app.models.user_model import User
 from app.schemas.user_schema import UserCreate
 
+from app.utils.security import hash_password
+from app.utils.security import verify_password
+
+
+
 def create_user(db: Session, user: UserCreate):
+
+    hashed_password = hash_password(user.password)
+
     new_user = User(
         name=user.name,
         email=user.email,
-        password=user.password
+        password=hashed_password
     )
 
     db.add(new_user)
@@ -18,3 +26,14 @@ def create_user(db: Session, user: UserCreate):
 
 def get_all_users(db: Session):
     return db.query(User).all()
+
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = db.query(User).filter(User.email == email).first()
+    if not user:
+        return None
+    
+    if not verify_password(password, user.password):
+        return None
+    
+    return user
